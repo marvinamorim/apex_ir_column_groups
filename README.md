cdafc409652d4cc0da31dc2ba19d1c6a3aa9dc04
marvinamorim <marvinamorim@gmail.com>
2021-08-17 16:37:57 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# Interactive Report Column Groups
+Oracle Apex plugin to show Column Groups over an Interactive Report like they show up using the Interactive Grid.
+This plugin uses the native Column Group setup for Interactive Report.
+
+
+## Instalation
+1. Import the file `dynamic_action_plugin_mvx_ir_coluns_group.sql` on your application.
+2. Configure the interactive report column groups on Page Design and asign columns to those groups.
+3. Create an After Refresh Dynamic Action and set as True Action `IR Column Groups [Plug-In]`.
+4. Set up the Static ID for the report, and put this ID on the plugin configuration field `Report Static ID`.
+5. Run the page and check it out.
+
+## Pre-requesit
+* The grouped columns should be ordered together
+
+## To-do
+* Use the saved report columns order, insted the page design columns order.
+* Add suport to custom CSS and click events for the groups.
---
+++ src/js/IRColumnsGroup.js
@@ -0,0 +1,53 @@
+const IRColumnGroups = function() {
+	let report = {};
+	async function init() {
+		const ajaxCallback = this.action.ajaxIdentifier;
+		report.id = this.action.attribute01;
+		console.log('Initing IRColumnsGroup for', "#"+report.id);
+		var args = { x01: report.id };
+		var columnGroups = await apex.server.plugin(ajaxCallback, args,{dataType:""});
+		groupedCols = groupColumns(columnGroups);
+		createGroupsHTML(groupedCols);
+	}
+
+	function groupColumns(ungroupedData) {
+		let groupCols = [];
+		ungroupedData.map(function(data) {
+			let i = groupCols.map(group => { return group.group }).indexOf(data.group);
+			if (i < 0) groupCols.push({group:data.group, columns:[data.column]})
+			else groupCols[i].columns.push(data.column);
+		});
+		return groupCols;
+	}
+
+	function createGroupsHTML(groupCols) {
+		var $tr = $("<tr></tr>");
+		var lastIndex = 1;
+		groupCols.map(thisGroup => {
+			var i = 1;
+			var firstGroupColumn = thisGroup.columns[0];
+			$(`#${report.id} .t-fht-thead .a-IRR-header`)
+			.each(function(){
+				let $th = $(this);
+				var $header = $th.find(".a-IRR-headerLink").length ?
+											$th.find(".a-IRR-headerLink") :
+											$th.find(".a-IRR-headerLabel");
+				let text = $($header.contents()[0]).text();
+				if (text == firstGroupColumn) return false;
+				else i++;
+			});
+			if (i > lastIndex) {
+				$tr.append($(`<td colspan="${i-lastIndex}"></td>`));
+				lastIndex = i;
+			}
+			colspan = thisGroup.columns.length;
+			lastIndex = i+colspan;
+			$tr.append($(`<td align="center" colspan="${colspan}">${thisGroup.group}</td>`));
+		});
+		$(`#${report.id} .t-fht-thead table`).prepend($tr.clone());
+	}
+
+	return {
+		init
+	}
+}();
